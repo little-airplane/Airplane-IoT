b974eb2ae865bf1a805fa6e9d1c4ea209331b866
小灰机 <kevin.lee4liminghao@gmail.com>
2020-02-29 15:19:19 +0800

    ?

--- Door/Door.Arduino/Door.Arduino.ino
+++ Door/Door.Arduino/Door.Arduino.ino
@@ -1,7 +1,31 @@
-int gmspin = 2;
-int 
+int gms = 2;
+int bell = 3;
+int human = 4;
+int belltest = 0;
 void setup()
 {
-    pinMode(gmspin, INPUT);
-    pinMode()
+    Serial.begin(115200);
+    pinMode(gms, INPUT);
+    pinMode(bell, INPUT);
+    pinMode(human, INPUT);
+}
+void loop()
+{
+    if (digitalRead(gms) == HIGH)
+    {
+        Serial.print("DOOR:CLOSE")
+    }
+    if (digitalRead(gms) == LOW)
+    {
+        Serial.print("DOOR:OPEN")
+    }
+    if (digitalRead(gms) == HIGH and belltest == 0)
+    {
+        Serial.print("DOOR:CLOSE")
+        int belltest = 1;
+    }
+    if (digitalRead(gms) == LOW and belltest ==  1)
+    {
+        int belltest = 0;
+    }
 }
